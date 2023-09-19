eb188db9dc2eb2ab3675283460552fb03a9c0312
Danilo <danilo.scardoso4@senacsp.edu.br>
2023-09-19 20:33:28 -0300

    Criação Classe Conexão

--- DoeComPaixao/Classes/Conexao.cs
+++ DoeComPaixao/Classes/Conexao.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +10,47 @@ namespace DoeComPaixao.Classes
 {
     internal class Conexao
     {
+        
+        #region Variáveis
+                                    
+        private static string _strConexao = @"Data Source=localhost\SQLEXPRESS;Initial Catalog=DoeComPaixao;Integrated Security=True";
+        public SqlConnection conexao = new SqlConnection(_strConexao);
+        public SqlCommand comando; 
+        public SqlDataAdapter da; 
+        public SqlDataReader dr; 
+        public DataSet ds; 
+        #endregion
+
+        #region Construtores
+
+        public Conexao(string query)
+        {
+            comando = new SqlCommand(query, conexao); 
+            da = new SqlDataAdapter(query, conexao); 
+            ds = new DataSet(); 
+        }
+
+        #endregion
+
+        #region Métodos
+
+        
+        public void AbrirConexao()
+        {
+            if (conexao.State == ConnectionState.Open)
+            {
+                conexao.Close();
+            }
+            conexao.Open();
+        }
+
+        public void FecharConexao()
+        {
+            if (conexao.State == ConnectionState.Open)
+            {
+                conexao.Close();
+            }
+        }
+        #endregion
     }
 }
